ff5a9798f3e3ac5bb99d3a775f05aeac68b67845
pjh5474 <warwarsn@gmail.com>
2023-02-14 01:45:14 +0900

    websocket part done!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,46 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
 const socket = new WebSocket(`ws://${window.location.host}`); // 서버에 접속
 
-socket.addEventListener("open", () => {
-	console.log("Connected to Server ✅"); // 서버에 접속되면 실행될 함수
-});
+function handleOpen() {
+	console.log("Connected to Server ✅");
+}
+socket.addEventListener("open", handleOpen); // 서버와 연결되면 실행될 함수
 
 socket.addEventListener("message", (message) => {
-	console.log("New Message: ", message.data, "from the server"); // 서버로부터 메시지를 받으면 실행될 함수
-});
+	const li = document.createElement("li");
+	li.innerText = message.data;
+	messageList.append(li);
+}); // 서버로부터 메시지를 받으면 실행될 함수
 
 socket.addEventListener("close", () => {
-	console.log("Disconnected from Server ❌"); // 서버와의 연결이 끊어지면 실행될 함수
-});
+	console.log("Disconnected from Server ❌");
+}); // 서버와의 연결이 끊어지면 실행될 함수
 
-setTimeout(() => {
-	socket.send("hello from the browser!");
-}, 10000); // 10초 후 서버로 메시지 전달
+function makeMessage(type, payload) {
+	const msg = { type, payload }; // 객체 생성
+	return JSON.stringify(msg); // JSON.stringify()를 사용하여 객체를 문자열로 변환
+}
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const input = messageForm.querySelector("input");
+	socket.send(makeMessage("new_message", input.value));
+	const li = document.createElement("li");
+	li.innerText = `You: ${input.value}`;
+	messageList.append(li);
+	input.value = ""; // input 초기화
+} // 서버로 메시지 전달
+
+function handleNickSubmit(event) {
+	event.preventDefault();
+	const input = nickForm.querySelector("input");
+	socket.send(makeMessage("nickname", input.value));
+	input.value = ""; // input 초기화
+} // 서버로 메시지 전달
+
+messageForm.addEventListener("submit", handleSubmit);
+
+nickForm.addEventListener("submit", handleNickSubmit);
