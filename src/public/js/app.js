09d1c11fb3d8121d11184fa93f21aa855ca412b5
pjh5474 <warwarsn@gmail.com>
2023-02-16 02:44:15 +0900

    Room Count & User Count & Admin Panel & LeaveRoomBtn

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,32 +1,17 @@
 const socket = io(); // socket.io-client를 import한 후 io()를 호출하면 socket.io 서버와 연결됨
 
+/* bind HTML elements */
 const welcomeDiv = document.getElementById("welcomeDiv");
 const roomForm = welcomeDiv.querySelector("form");
-
 const roomDiv = document.getElementById("roomDiv");
+const leaveRoomBtn = roomDiv.querySelector("#leaveRoomBtn");
 
 roomDiv.hidden = true;
 
 let roomName = "";
 
-function handleRoomSubmit(event) {
-	event.preventDefault();
-	const roomInput = roomForm.querySelector("#roomInput");
-	const nicknameInput = roomForm.querySelector("#nicknameInput");
-	if (nicknameInput.value === "") {
-		socket.emit("nickname", "Anonymous");
-	} else {
-		socket.emit("nickname", nicknameInput.value);
-	}
-
-	socket.emit("enter_room", roomInput.value, showRoom); // socket.io-client에서 이벤트를 보낼 수 있음
-	// emit으로 unlimited argumnets를 보낼 수 있음
-	// 끝날 때 실행되는 함수를 보내려면 마지막 인자로 보내야 한다
-	roomName = roomInput.value;
-	roomInput.value = "";
-}
-
-roomForm.addEventListener("submit", handleRoomSubmit);
+/* bind HTML elements */
+/*           handle messages function        */
 
 function addMessage(message) {
 	const ul = roomDiv.querySelector("ul");
@@ -45,21 +30,70 @@ function handleMessageSubmit(event) {
 	input.value = "";
 }
 
-function showRoom() {
+socket.on("new_message", addMessage); // (msg) => addMessage(msg)와 같음
+
+/*           handle messages function        */
+/*           Leave and Join Room function        */
+
+function handleRoomSubmit(event) {
+	event.preventDefault();
+	const roomInput = roomForm.querySelector("#roomInput");
+	const nicknameInput = roomForm.querySelector("#nicknameInput");
+	if (nicknameInput.value === "") {
+		socket.emit("nickname", "Anonymous");
+	} else {
+		socket.emit("nickname", nicknameInput.value);
+	}
+
+	socket.emit("enter_room", roomInput.value, showRoom); // socket.io-client에서 이벤트를 보낼 수 있음
+	// emit으로 unlimited argumnets를 보낼 수 있음
+	// 끝날 때 실행되는 함수를 보내려면 마지막 인자로 보내야 한다
+	roomName = roomInput.value;
+	roomInput.value = "";
+}
+
+function handleLeaveRoom(event) {
+	event.preventDefault();
+	socket.emit("leaveRoom", roomName);
+	roomDiv.hidden = true;
+	welcomeDiv.hidden = false;
+}
+
+roomForm.addEventListener("submit", handleRoomSubmit);
+leaveRoomBtn.addEventListener("click", handleLeaveRoom);
+
+function setRoomTitle(roomName, userCount) {
+	const h3 = roomDiv.querySelector("h3");
+	h3.innerText = `Room ${roomName} (${userCount})`;
+}
+
+function showRoom(userCount) {
 	welcomeDiv.hidden = true;
 	roomDiv.hidden = false;
-	const h3 = roomDiv.querySelector("h3");
-	h3.innerText = `Room ${roomName}`;
+	setRoomTitle(roomName, userCount);
 	const msgForm = roomDiv.querySelector("#msgForm");
 	msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
-socket.on("welcome", (nickname) => {
+socket.on("welcome", (nickname, userCount) => {
+	setRoomTitle(roomName, userCount);
 	addMessage(`${nickname} Joined!`);
 });
 
-socket.on("bye", (nickname) => {
+socket.on("bye", (nickname, userCount) => {
+	setRoomTitle(roomName, userCount);
 	addMessage(`${nickname} Left!`);
 });
 
-socket.on("new_message", addMessage); // (msg) => addMessage(msg)와 같음
+socket.on("room_change", (publicRooms) => {
+	const roomList = welcomeDiv.querySelector("ul");
+	roomList.innerHTML = "";
+	if (publicRooms.length === 0) {
+		return;
+	}
+	publicRooms.forEach((publicRoom) => {
+		const li = document.createElement("li");
+		li.innerText = publicRoom;
+		roomList.append(li);
+	});
+});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -13,28 +14,65 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app); // http 서버 생성 후 접근 가능한 express app을 전달
-const wsServer = SocketIO(httpServer); // http 서버를 SocketIO에 전달
+const wsServer = new Server(httpServer, {
+	cors: {
+		origin: ["https://admin.socket.io"],
+		credentials: true,
+	},
+}); // http 서버를 SocketIO에 전달
+
+instrument(wsServer, {
+	auth: false,
+});
+
+function publicRooms() {
+	const {
+		sockets: {
+			adapter: { sids, rooms },
+		},
+	} = wsServer;
+	const publicRooms = [];
+	rooms.forEach((_, key) => {
+		if (sids.get(key) === undefined) {
+			publicRooms.push(key);
+		}
+	});
+	return publicRooms;
+}
+
+function countRoomUsers(roomName) {
+	return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
 
 wsServer.on("connection", (socket) => {
+	wsServer.sockets.emit("room_change", publicRooms());
 	socket["nickname"] = "Anonymous";
 	socket.onAny((event) => {
+		console.log(wsServer.sockets.adapter);
 		console.log(`Socket Event: ${event}`);
 	});
 	socket.on("enter_room", (roomName, showRoom) => {
 		socket.join(roomName);
-		showRoom();
-		socket.to(roomName).emit("welcome", socket.nickname);
+		showRoom(countRoomUsers(roomName));
+		socket
+			.to(roomName)
+			.emit("welcome", socket.nickname, countRoomUsers(roomName));
 		// socket.to(roomName)으로 특정 room에 있는 socket에게만 이벤트를 보낼 수 있음
 		// 특정 room의 자신을 제외한 모든 socket에게 welcome 이벤트 전달
+		wsServer.sockets.emit("room_change", publicRooms());
 	}); // socket.io 서버에서 이벤트를 받고, 이벤트는 프론트엔드에서 실행됨
 	// frontend emit 에서 전달한 function을 backend에서 제어할 수 있음
 
 	socket.on("disconnecting", () => {
 		socket.rooms.forEach((room) =>
-			socket.to(room).emit("bye", socket.nickname)
+			socket.to(room).emit("bye", socket.nickname, countRoomUsers(room) - 1)
 		);
 		// socket.rooms는 socket이 접속한 모든 room의 이름을 담고 있음
 		// socket.rooms.forEach(room => socket.to(room).emit("bye"));로 모든 room에 bye 이벤트를 보낼 수 있음
+	});
+
+	socket.on("disconnect", () => {
+		wsServer.sockets.emit("room_change", publicRooms());
 	});
 
 	socket.on("new_message", (msg, roomName, done) => {
@@ -43,6 +81,11 @@ wsServer.on("connection", (socket) => {
 	});
 
 	socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+	socket.on("leaveRoom", (roomName) => {
+		socket.leave(roomName);
+		socket.to(roomName).emit("bye", socket.nickname, countRoomUsers(roomName));
+		wsServer.sockets.emit("room_change", publicRooms());
+	});
 });
 
 const handleListen = () => console.log("Listening on http://localhost:3000"); // 서버가 실행되면 실행될 함수
